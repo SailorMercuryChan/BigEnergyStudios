faea24823aa4bcfb1bc41dc77bc19f8f0b9fa2f6
Ethan Willinger <45436341+SailorMercuryChan@users.noreply.github.com>
2018-11-28 14:24:35 -0600

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -1,30 +1,26 @@
 ﻿using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
 
-    public GameObject player;
-    public GameObject enemy;
-
-    private int score;
-    private Rigidbody rb;
-
-    private void Start()
     {
-        rb = GetComponent<RigidBody>();
+    public GameObject player;
+    public Enemy dick;
+
+        private rigidbody rb;
+        
+    [SerializeField] private int score;
+
+    private void Start()t
+    {
+        
+
     }
 
     private void Update()
     {
-        rb.Translate(Vector3.up);
+        if (left arrrow is pressed)
 
-        if(something)
-        {
-            something();
-        }
-
-        for(int i = 0; i < somethijng; i++)
-        {
-
-        }
+        translate.
     }
+
 }
